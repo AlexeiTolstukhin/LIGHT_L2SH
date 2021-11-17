42e83d178dddaa50e2687e989bd41a13c67244b0
alexei_v_tolstukhin <tolstukhin.av@sch2.ru>
2021-11-17 14:51:45 +0300

    17.11.2021 ДЗ сделайте форму входа на сайт authorization.html (username, password, две кнопки "Войти" и "Зарегистрироваться"

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/sign-up', methods=['POST'])
+def sign_up_user():
+    error = 0
+    btn = request.form['action']  # определяем какая нажата кнопка
+    if btn == 'signUp':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirmPassword']
+        if password != confirm_password:
+            error = 1
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        # В перспективе нужно реализовать проверку пароля на ошибки, а также запись пользователя в Базу данных.
+        print(email, username, password, confirm_password, first_name, last_name)
+        return render_template(
+            'registration.html',
+            error=error
+        )
+    elif btn == 'cancel':
+        return render_template(
+            'registration.html',
+            error=error
+        )
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template(
+        'registration.html'
+    )
 
 
 if __name__ == '__main__':
